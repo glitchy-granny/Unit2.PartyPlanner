1c6c434bba41e8c949c3d333d0c4be4c096b919f
Pamela <pdaniels@Desktop-HomeOffice.localdomain>
2023-11-06 20:40:13 -0500

    rewrite code because of function misplacement

--- index.js
+++ index.js
@@ -3,32 +3,33 @@ const EVENTS_ENDPOINT = `${BASE_URL}/events`;
 
 const PARTY_LIST = document.getElementById('partyList');
 
-const events = [];
-
 function renderEvents(events) {
+
+    PARTY_LIST.innerHTML = '';
     for (let event of events) {
         let eventListItem = document.createElement("li");
-        eventListItem.textContent = 'Name: ${event name} Description ${event.description} Date: ${event.date} Location: ${event.location}';
+
+        eventListItem.textContent = `Name: ${event.name} Description: ${event.description} Date: ${event.date} Location: ${event.location}`;
         PARTY_LIST.append(eventListItem);
     }
-
-    async function fetchEvents() {
-        try {
-            const response = await fetch(EVENTS_ENDPOINT);
-            if (!response.ok) {
-                console.log("API error", response.status);
-                return;
-            }
-            const jsonResponse = await response.json();
-            const events = jsonResponse.data;
-            renderEvents(events);
-
-        } catch (error) {
-            console.error(error);
-
-        }
-    }
-
-    fetchEvents;
-
 }
+
+
+async function fetchEvents() {
+    try {
+        const response = await fetch(EVENTS_ENDPOINT);
+        if (!response.ok) {
+            console.log("API error", response.status);
+            return;
+        }
+        const jsonResponse = await response.json();
+        const events = jsonResponse.data;
+        renderEvents(events);
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+fetchEvents();
