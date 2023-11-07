8fd1131d8640ee6af2a94162421e94904fc66b7d
Pamela <pdaniels@Desktop-HomeOffice.localdomain>
2023-11-06 20:23:06 -0500

    added code to index.js file

--- index.js
+++ index.js
@@ -3,25 +3,7 @@ const EVENTS_ENDPOINT = `${BASE_URL}/events`;
 
 const PARTY_LIST = document.getElementById('partyList');
 
-async function fetchEvents() {
-    try {
-        const response = await fetch(EVENTS_ENDPOINT);
-        if (!response.ok) {
-            console.log("API error", response.status);
-            return;
-        }
-        const jsonResponse = await response.json();
-        const events = jsonResponse.data;
-        renderEvents(events);
-
-    } catch (error) {
-        console.error(error);
-
-    }
-}
-
-fetchEvents;
-
+const events = [];
 
 function renderEvents(events) {
     for (let event of events) {
@@ -29,4 +11,24 @@ function renderEvents(events) {
         eventListItem.textContent = 'Name: ${event name} Description ${event.description} Date: ${event.date} Location: ${event.location}';
         PARTY_LIST.append(eventListItem);
     }
+
+    async function fetchEvents() {
+        try {
+            const response = await fetch(EVENTS_ENDPOINT);
+            if (!response.ok) {
+                console.log("API error", response.status);
+                return;
+            }
+            const jsonResponse = await response.json();
+            const events = jsonResponse.data;
+            renderEvents(events);
+
+        } catch (error) {
+            console.error(error);
+
+        }
+    }
+
+    fetchEvents;
+
 }
