355d6f5c3a3e8c680dbfb3e258c56037f4fb86f3
Pamela <pdaniels@Desktop-HomeOffice.localdomain>
2023-11-06 20:07:46 -0500

    third commit modified index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const BASE_URL = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-ACC-ET-WEB-PT-B';
+const EVENTS_ENDPOINT = `${BASE_URL}/events`;
+
+const PARTY_LIST = document.getElementById('partyList');
+
+async function fetchEvents() {
+    try {
+        const response = await fetch(EVENTS_ENDPOINT);
+        if (!response.ok) {
+            console.log("API error", response.status);
+            return;
+        }
+        const jsonResponse = await response.json();
+        const events = jsonResponse.data;
+        renderEvents(events);
+
+    } catch (error) {
+        console.error(error);
+
+    }
+}
+
+fetchEvents;
+
+
+function renderEvents(events) {
+    for (let event of events) {
+        let eventListItem = document.createElement("li");
+        eventListItem.textContent = 'Name: ${event name} Description ${event.description} Date: ${event.date} Location: ${event.location}';
+        PARTY_LIST.append(eventListItem);
+    }
+}
