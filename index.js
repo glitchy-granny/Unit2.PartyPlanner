01bf05cd4738e5fb50d9692823be477bb8a38384
Pamela <pdaniels@Desktop-HomeOffice.localdomain>
2023-11-16 20:26:12 -0500

    added party createPartyCard function

--- index.js
+++ index.js
@@ -3,6 +3,43 @@ const EVENTS_ENDPOINT = `${BASE_URL}/events`;
 
 const PARTY_LIST = document.getElementById('partyList');
 
+const events = [];
+
+function createPartyCard(title, date, address, description) {
+
+    const PARTY_CARDS = document.querySelector("#cards");
+    const PARTY_CARD = document.createElement("div");
+    PARTY_CARD.classList.add('card')
+
+    const PARTY_CARD_TITLE = document.createElement("h2");
+    PARTY_CARD_TITLE.classList.add('title');
+    PARTY_CARD_TITLE.textContent = title;
+
+    const PARTY_CARD_DATE = document.createElement("p");
+    PARTY_CARD_DATE.classList.add('date');
+    PARTY_CARD_DATE.textContent = date;
+
+    const PARTY_CARD_ADDRESS = document.createElement("p");
+    PARTY_CARD_ADDRESS.classList.add('address')
+    PARTY_CARD_ADDRESS.textContent = address;
+
+    const PARTY_CARD_DESCRIPTION = document.createElement("p");
+    PARTY_CARD_DESCRIPTION.classList.add('description');
+    PARTY_CARD_DESCRIPTION.textContent = description;
+
+}
+
+
+function renderEvents(events) {
+
+    PARTY_LIST.innerHTML = '';
+    for (let event of events) {
+        let eventListItem = document.createElement("li");
+
+        eventListItem.textContent = `Name: ${event.name} Description: ${event.description} Date: ${event.date} Location: ${event.location}`;
+        PARTY_LIST.append(eventListItem);
+    }
+}
 
 async function fetchEvents() {
     try {
@@ -23,16 +60,7 @@ async function fetchEvents() {
 
 fetchEvents();
 
-function renderEvents(events) {
 
-    PARTY_LIST.innerHTML = '';
-    for (let event of events) {
-        let eventListItem = document.createElement("li");
-
-        eventListItem.textContent = `Name: ${event.name} Description: ${event.description} Date: ${event.date} Location: ${event.location}`;
-        PARTY_LIST.append(eventListItem);
-    }
-}
 
 
 
